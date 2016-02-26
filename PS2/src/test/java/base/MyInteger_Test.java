63e8bb1bd83fd5fd3c4fd6a50b0bdf29ea799d66
xbox3 <xbox3@DESKTOP-073L0L3>
2016-02-25 20:04:34 -0500

    Commit

--- PS2/src/test/java/base/MyInteger_Test.java
+++ PS2/src/test/java/base/MyInteger_Test.java
@@ -9,6 +9,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MyInteger_Test {
+	MyInteger a;
+	MyInteger b;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -20,15 +22,40 @@ public class MyInteger_Test {
 
 	@Before
 	public void setUp() throws Exception {
+		a = new MyInteger(10);
+		b = new MyInteger(11);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		a = null;
+		b = null;
 	}
 
 	@Test
 	public void test() {
-		assertTrue(1==1);
+		assertTrue(a.isEven());
+		assertFalse(b.isEven());
+		assertTrue(b.isOdd());
+		assertFalse(a.isOdd());
+		assertTrue(b.isPrime());
+		assertFalse(a.isPrime());
+		assertTrue(a.isEven(100));
+		assertFalse(b.isEven(1));
+		assertTrue(b.isOdd(111));
+		assertFalse(a.isOdd(12));
+		assertTrue(b.isPrime(17));
+		assertFalse(a.isPrime(90));
+		assertTrue(a.isEven(a));
+		assertFalse(b.isEven(b));
+		assertTrue(b.isOdd(b));
+		assertFalse(a.isOdd(a));
+		assertTrue(b.isPrime(b));
+		assertFalse(a.isPrime(a));
+		assertTrue(a.equals(10));
+		assertFalse(a.equals(11));
+		assertTrue(a.equals(a));
+		assertFalse(a.equals(b));
 	}
 
 }
